627b1b2c2def4e4aec71249e5debebe609422d13
cristhian ochoa <cristhho@gmail.com>
2020-06-26 00:54:32 -0500

    profile screen and widgets

--- lib/user/ui/screens/profile.dart
+++ lib/user/ui/screens/profile.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'profile_header.dart';
+import '../widgets/profile_background.dart';
+import '../widgets/profile_places_list.dart';
 
 class Profile extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.indigo,
+    return Stack(
+      children: <Widget>[
+        ProfileBackground(),
+        ListView(
+          children: <Widget>[
+            ProfileHeader(),
+            ProfilePlacesList()
+
+          ],
+        ),
+      ],
     );
   }
 
