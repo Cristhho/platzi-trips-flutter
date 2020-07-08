b5d69346cb1597b10ec4c4b2ca8455a5d9caa630
cristhian ochoa <cristhho@gmail.com>
2020-07-07 23:27:46 -0500

    custom text input

--- lib/place/ui/screens/add_place-screen.dart
+++ lib/place/ui/screens/add_place-screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import '../../../widgets/text_input.dart';
 import '../../../widgets/title_header.dart';
 import '../../../widgets/gradient_back.dart';
 
@@ -16,6 +17,10 @@ class AddPlaceScreen extends StatefulWidget {
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   @override
   Widget build(BuildContext context) {
+
+    final _controllerTitlePlace = TextEditingController();
+    final _controllerDescriptionPlace = TextEditingController();
+
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -43,6 +48,30 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                 )
               )
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 120.0, bottom: 20.0),
+            child: ListView(
+              children: <Widget>[
+                Container(
+
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInput(
+                    hintText: "Title",
+                    inputType: null,
+                    controller: _controllerTitlePlace,
+                  ),
+                ),
+                TextInput(
+                  hintText: "Description",
+                  inputType: TextInputType.multiline,
+                  maxLines: 4,
+                  controller: _controllerDescriptionPlace,
+                )
+              ],
+            ),
           )
         ],
       ),
